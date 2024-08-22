c87545389d79b65df2b310e3694b04c2d0f31a52
O-DevOps-1 <ofattahserir@gmail.com>
2024-08-22 19:32:59 +0200

    Update taskController.js

---
+++ api/controllers/taskController.js
@@ -0,0 +1,40 @@
+const Task = require('../models/Task');
+
+// from the models directory
+
+exports.createTask = async (req, res) => {
+  try {
+    const task = new Task({ ...req.body, user: req.user._id });
+    await task.save();
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }};
+
+exports.getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({ user: req.user._id });
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+  
+};
+
+exports.updateTask = async (req, res) => {
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!task) return res.status(404).json({ message: 'Task not found' });
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) return res.status(404).json({ message: 'Task not found' });
+    res.json({ message: 'Task deleted' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });}};
