c6fab666679de451d61a2696ea4ff2f71d375903
O-DevOps-1 <ofattahserir@gmail.com>
2024-08-22 19:54:13 +0200

    Create validate.js

---
+++ api/middlewares/validate.js
@@ -0,0 +1,9 @@
+const Joi = require('joi');
+
+const validate = (schema) => (req, res, next) => {
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).json({ error: error.details[0].message });
+  next();
+};
+
+module.exports = validate;
