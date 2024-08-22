473e6714c9823e618ccbdf46dc39810c8865f9c1
O-DevOps-1 <ofattahserir@gmail.com>
2024-08-22 20:18:57 +0200

    Update taskRoutes.js

--- api/routes/taskRoutes.js
+++ api/routes/taskRoutes.js
@@ -3,17 +3,17 @@ const {
   createTask,
   getTasks,
   updateTask,
-  deleteTask,
+  addAttachment,
 } = require('../controllers/taskController');
 const authMiddleware = require('../middlewares/authMiddleware');
-
+const validate = require('../middlewares/validate');
+const taskValidation = require('../validations/taskValidation');
 const router = express.Router();
 
-router.use(authMiddleware);
-
-router.post('/', createTask);
-router.get('/', getTasks);
-router.put('/:id', updateTask);
-router.delete('/:id', deleteTask);
+router.use(authMiddleware); // authentication middleware
+router.post('/', validate(taskValidation), createTask); // route to create a task
+router.get('/', getTasks); // route to get all tasks
+router.put('/:id', validate(taskValidation), updateTask);
+router.post('/:id/attachments', addAttachment); // new route to add attachments
 
 module.exports = router;
