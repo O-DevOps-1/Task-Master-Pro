0e60f13ddff1a158b2bb2ae148cab8a2aaf2e722
O-DevOps-1 <ofattahserir@gmail.com>
2024-08-22 19:22:01 +0200

    Update authController.js

---
+++ api/controllers/authController.js
@@ -0,0 +1,33 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+
+
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: 'User already exists,try again' });
+
+    user = new User({ name, email, password });
+    await user.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.status(201).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user || !(await user.matchPassword(password)))
+      return res.status(400).json({ message: 'Invalid credentials' });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }};
