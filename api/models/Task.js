33b63ac7f54e8a1815096fadad06769d15fd755d
O-DevOps-1 <ofattahserir@gmail.com>
2024-08-22 20:05:34 +0200

    Update Task.js

--- api/models/Task.js
+++ api/models/Task.js
@@ -1,21 +1,18 @@
 const mongoose = require('mongoose');
 
-// define schema's task
 const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String },
   status: { type: String, enum: ['todo', 'in-progress', 'done'], default: 'todo' },
   priority: { type: String, enum: ['low', 'medium', 'high'], default: 'medium' },
   deadline: { type: Date },
-  user: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Asignar a múltiples usuarios
+  user: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Assign to multiple users
   project: { type: mongoose.Schema.Types.ObjectId, ref: 'Project' },
   subtasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }],
   dependencies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }],
   tags: [{ type: String }],
-  attachments: [{ type: String }], // URLs de archivos adjuntos
+  attachments: [{ type: String }], // URLs of attached files
 });
 
-// create task modelling
 const Task = mongoose.model('Task', taskSchema);
-
 module.exports = Task;
