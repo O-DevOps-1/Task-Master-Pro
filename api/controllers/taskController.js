ba77d84fbfa2371a205068c3c389a2b8d1324e18
O-DevOps-1 <ofattahserir@gmail.com>
2024-08-22 20:06:17 +0200

    Update taskController.js

--- api/controllers/taskController.js
+++ api/controllers/taskController.js
@@ -1,55 +1,43 @@
 const Task = require('../models/Task');
 
-// from the models directory
-
 exports.createTask = async (req, res) => {
   try {
     const task = new Task({ ...req.body, user: req.user._id });
     await task.save();
     res.status(201).json(task);
   } catch (error) {
+    res.status(500).json({ message: error.message }); }};
+
+
+
+exports.updateTask = async (req, res) => {
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!task) return res.status(404).json({ message: 'Task not found' });
+
+    // Emit real-time updates
+    req.io.emit('taskUpdated', task);
+    res.json(task);
+  } catch (error) {
     res.status(500).json({ message: error.message });
   }};
 
 exports.getTasks = async (req, res) => {
   try {
-    const tasks = await Task.find({ user: req.user._id });
+    const tasks = await Task.find({ user: req.user._id }).populate('subtasks').populate('dependencies');
     res.json(tasks);
   } catch (error) {
     res.status(500).json({ message: error.message });
-  }
-  
-};
+  }};
 
-exports.updateTask = async (req, res) => {
+exports.addAttachment = async (req, res) => {
   try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const task = await Task.findById(req.params.id);
     if (!task) return res.status(404).json({ message: 'Task not found' });
+
+    task.attachments.push(req.body.attachmentUrl);
+    await task.save();
     res.json(task);
   } catch (error) {
     res.status(500).json({ message: error.message });
-  }
-};
-
-exports.deleteTask = async (req, res) => {
-  try {
-    const task = await Task.findByIdAndDelete(req.params.id);
-    if (!task) return res.status(404).json({ message: 'Task not found' });
-    res.json({ message: 'Task deleted' });
-  } catch (error) {
-    res.status(500).json({ message: error.message });}}; 
-
-const io = require('../server').io;
-
-exports.updateTask = async (req, res) => {
-  try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (!task) return res.status(404).json({ message: 'Task not found' });
-
-    io.emit('taskUpdated', task);  // broadcsst update to all connected clients
-    res.json(task);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
+  }};
