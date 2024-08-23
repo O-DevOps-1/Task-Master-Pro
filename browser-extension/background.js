263ad8baf481c16a1ba11f5c510ee2369f989262
O-DevOps-1 <ofattahserir@gmail.com>
2024-08-23 19:56:46 +0200

    Update background.js

--- browser-extension/background.js
+++ browser-extension/background.js
@@ -1,14 +1,26 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color: '#3aa757' }, () => {
-    console.log('The color is green.');
+  chrome.storage.sync.set({ color: '#3aa757', darkMode: false }, () => {
+    console.log('The color is green and dark mode is disabled.'); });
+
+  chrome.alarms.create('remindDeadline', { periodInMinutes: 1 });
+
+  chrome.action.onClicked.addListener((tab) => {
+    chrome.notifications.create({
+      type: 'basic',
+      iconUrl: 'icons/icon48.png',
+      title: 'Task Manager',
+      message: 'Stay productive! You have tasks to complete.',
+    });
   });
+
 });
 
-chrome.action.onClicked.addListener((tab) => {
-  chrome.notifications.create({
-    type: 'basic',
-    iconUrl: 'icons/icon48.png',
-    title: 'Task Manager',
-    message: 'Stay productive! You have tasks to complete.',
-  });
-});
+chrome.alarms.onAlarm.addListener(alarm => {
+  if (alarm.name === 'remindDeadline') {
+    checkTasksForReminders();
+  }});
+
+function checkTasksForReminders() {
+  // this would involve fetching tasks and checking deadlines, sending notifications accordingly, etcetera
+  console.log("Checking tasks for upcoming deadlines...");
+}
