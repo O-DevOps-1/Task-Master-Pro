deb2d2b5e756fcbb69b2d90674602e0847a3df03
O-DevOps-1 <ofattahserir@gmail.com>
2024-08-24 11:21:23 +0200

    Update options.js

--- browser-extension/options/options.js
+++ browser-extension/options/options.js
@@ -1,19 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
   const darkModeCheckbox = document.getElementById('darkMode');
   const notificationsCheckbox = document.getElementById('notifications');
+  const syncWithGoogleCalendarCheckbox = document.getElementById('syncWithGoogleCalendar');
 
-  // loafs and sets current settings
-  chrome.storage.sync.get(['darkMode', 'notifications'], (result) => {
+  chrome.storage.sync.get(['darkMode', 'notifications', 'syncWithGoogleCalendar'], (result) => {
     darkModeCheckbox.checked = result.darkMode || false;
     notificationsCheckbox.checked = result.notifications || false;
+    syncWithGoogleCalendarCheckbox.checked = result.syncWithGoogleCalendar || false;
   });
 
-  // saving the elected settings
   document.getElementById('optionsForm').addEventListener('submit', (event) => {
     event.preventDefault();
     chrome.storage.sync.set({
       darkMode: darkModeCheckbox.checked,
       notifications: notificationsCheckbox.checked,
+      syncWithGoogleCalendar: syncWithGoogleCalendarCheckbox.checked,
     }, () => {
       alert('Settings saved!');
     });
