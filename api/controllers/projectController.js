8979bece86e8609d97c3093e11500efa8e715ad4
O-DevOps-1 <ofattahserir@gmail.com>
2024-08-30 11:53:37 +0200

    Update projectController.js

--- api/controllers/projectController.js
+++ api/controllers/projectController.js
@@ -1,62 +1,39 @@
 const Project = require('../models/Project');
+const Task = require('../models/Task');
 
-const createProject = async (req, res) => {
-    try {
-        const project = new Project(req.body);
-        await project.save();
-        res.status(201).json(project);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }};
+exports.createProject = async (req, res) => {
+  try {
+    const project = new Project(req.body);
+    await project.save();
+    res.status(201).json(project);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }};
 
-const getProjects = async (req, res) => {
-    try {
-        const projects = await Project.find();
-        res.status(200).json(projects);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+exports.getProjects = async (req, res) => {
+  try {
+    const projects = await Project.find({ users: req.user._id }).populate('tasks');
+    res.json(projects);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }};
+
+exports.updateProject = async (req, res) => {
+  try {
+    const project = await Project.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!project) return res.status(404).json({ message: 'Project not found' });
+    res.json(project);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
-const getProjectById = async (req, res) => {
-    try {
-        const project = await Project.findById(req.params.id);
-        if (!project) {
-            return res.status(404).json({ message: 'Project not found' });
-        }
-        res.status(200).json(project);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
-
-const updateProject = async (req, res) => {
-    try {
-        const project = await Project.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        if (!project) {
-            return res.status(404).json({ message: 'Project not found' });
-        }
-        res.status(200).json(project);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-};
-
-const deleteProject = async (req, res) => {
-    try {
-        const project = await Project.findByIdAndDelete(req.params.id);
-        if (!project) {
-            return res.status(404).json({ message: 'Project not found' });
-        }
-        res.status(200).json({ message: 'Project deleted' });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
-
-module.exports = {
-    createProject,
-    getProjects,
-    getProjectById,
-    updateProject,
-    deleteProject };
+exports.deleteProject = async (req, res) => {
+  try {
+    const project = await Project.findByIdAndDelete(req.params.id);
+    if (!project) return res.status(404).json({ message: 'Project not found' });
+    await Task.deleteMany({ project: project._id });
+    res.json({ message: 'Project deleted' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }};
