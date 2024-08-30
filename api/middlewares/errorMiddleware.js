58f1ac80006e86b444290fae3ce5f16805707e8d
O-DevOps-1 <ofattahserir@gmail.com>
2024-08-30 11:53:59 +0200

    Update errorMiddleware.js

--- api/middlewares/errorMiddleware.js
+++ api/middlewares/errorMiddleware.js
@@ -1,9 +1,6 @@
-function errorHandler(err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).json({
-        message: 'Something went wrong!',
-        error: err.message
-    });
-}
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'An error occurred', error: err.message });
+};
 
 module.exports = errorHandler;
